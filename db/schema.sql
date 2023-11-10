735367fd57b9817acb4270b79397769241f024fe
Alux <plutocoding@gmail.com>
2023-11-09 21:38:00 -0500

    Setting up tables

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,44 @@ DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
 USE employees_db;
+
+-- See/check if database in use --
+SELECT DATABASE();
+
+-- Creates the table "department" within employees_db --
+CREATE TABLE department (
+  -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
+  id INT NOT NULL,
+  -- Makes a string column called "name" which cannot contain null --
+  name VARCHAR(100) NOT NULL
+  -- Department Name
+);
+
+CREATE TABLE role (
+    id INT NOT NULL PRIMARY KEY,
+
+    title VARCHAR(30),
+    -- to hold role title
+
+    salary DECIMAL,
+    -- to hold role salary
+
+    department_id INT,
+    -- to hold reference to department role belongs to
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL PRIMARY KEY,
+
+    first_name VARCHAR(30), 
+    -- to hold employee first name
+
+    last_name VARCHAR(30),
+    -- to hold employee last name
+
+    role_id INT,
+    -- to hold reference to employee role
+
+    manager_id INT,
+    --  to hold reference to another employee that is the manager of the current employee (`null` if the employee has no manager)
+);
