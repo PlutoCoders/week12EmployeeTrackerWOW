86d0fa1fc81b546d5f8e60fa2ead8ae6e2fcd84f
Alux <plutocoding@gmail.com>
2024-01-15 21:30:09 -0500

    add role functionality, now using the capwords helper for adding emplyee and new role

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Connect to database
-const db = mysql.createConnection(
+const db = mySQL2.createConnection(
   {
     host: "localhost",
     // MySQL username,
@@ -27,6 +27,27 @@ const db = mysql.createConnection(
     console.log(`Connected to the employees_db database`, database);
   }
 );
+
+const roleLevels = {
+  Chief: 1,
+  Magistrate: 2,
+  Warden: 3,
+  Minister: 4,
+  King: 5,
+}
+
+// Used in places such as the addEmployee, so that the names are structured nicely, since users won't always capitalize every first letter of a name.
+capWords = (str) => {
+  return str.replace(/\b\w/g, (match) => {
+    return match.toUpperCase();
+  });
+}
+
+// (Chief, 100000, 1),
+// (Magistrate, 75000, 2),
+// (Warden, 85000, 3),
+// (Minister, 90000, 4),
+// (King, 80000, 5);
 
 // mysql -y root -p
 // mySQL 
@@ -120,13 +141,14 @@ const viewDepartments = ( res = false, server = false, sortByNewestFirst = false
     } else {
       if (departments.length > 0) {
         let databaseDepartments = departments.map(dep => new Department(dep));
+        if (sortByNewestFirst == true) databaseDepartments = databaseDepartments.reverse();
         console.table(databaseDepartments);
       } else {
         console.log(`No Departments to view!`);
       }
       setTimeout(() => {
         startMenu();
-      }, 4000)
+      }, 3000)
     }
   });
 }
@@ -293,6 +315,105 @@ const addDepartment = ( req = false, res = false, server = false ) => {
   });
 };
 
+const addRole = ( req = false, res = false, server = false ) => {
+  const sql = `SELECT * FROM roles; SELECT * FROM employees; SELECT * FROM departments;`;
+  db.query(sql, (error, allDataFromTables) => {
+    error ? console.log(error) : true; 
+
+    let [ roles, employees, departments ] = allDataFromTables;
+
+    let expandedRoles = roles.map(rol => {
+      let thisRolesDepartment = departments.find(dep => dep.id == rol.department_id);
+      return {
+        ...new Role(rol),
+        department_name: thisRolesDepartment.name
+      }
+    })
+
+    let expandedEmployees = employees.map(emp => {
+      let isManager = emp.role_id == roleLevels.Manager;
+      let managerOfEmployee = employees.find(em => emp.manager_id == em.id);
+      let thisEmployeesRole = expandedRoles.find(rol => rol.id == emp.role_id);
+      let thisEmployeesDepartment = departments.find(dep => dep.id == thisEmployeesRole.department_id);
+
+      return {
+        ...new Employee(emp),
+        salary: parseFloat(thisEmployeesRole.salary).toLocaleString(`en-US`),
+        job_title: thisEmployeesRole.title,
+        department_id: thisEmployeesDepartment.id,
+        department_name: thisEmployeesDepartment.name,
+        fullName: `${emp.first_name} ${emp.last_name}`,
+        manager_id: isManager ? null : managerOfEmployee.id,
+        manager_name: isManager ? null : `${managerOfEmployee.first_name} ${managerOfEmployee.last_name}`,
+      }
+    })
+
+    let managers = expandedEmployees.filter(emp => emp.role_id == roleLevels.Manager);
+    let managerNames = managers.map(emp => emp.fullName);
+    let deparmentNames = departments.map(dep => dep.name);
+
+    inquirer.prompt([
+      {
+        type: `input`,
+        name: `roleName`,
+        message: `What is the name of the Role?`,
+      },
+      {
+        type: `input`,
+        name: `salary`,
+        message: `What is the Salary for this position?`
+      },
+      {
+        name: `department`,
+        type: `list`,
+        choices: deparmentNames
+      }
+    ]).then(response => {
+      let salary = response.salary;
+      let roleName = response.roleName;
+      let currentCapitalizedRoleName = capWords(roleName);
+      let roleNames = expandedRoles.map(rol => rol.title);
+      let selectedDepartment = response.department;
+      let selectedDepartmentID = departments.find(dep => dep.name == selectedDepartment).id;
+
+      if (roleNames.includes(currentCapitalizedRoleName)) {
+        console.log(`Role Exists..`);
+        setTimeout(() => {
+          startMenu();
+        }, 3000);
+        return;
+      } else {
+        const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?);`;
+        if (server == true) {
+          let { body } = req;
+          const params = [body.roleName, body.salary, body.selectedDepartmentID];
+          db.query(sql, params, (err, result) => {
+            if (err) {
+              res.status(400).json({ error: err.message });
+              return;
+            }
+            res.json({
+              message: "Added Role!:",
+              data: body,
+            });
+          });
+        } else {
+          const params = [currentCapitalizedRoleName, salary, selectedDepartmentID];
+          db.query(sql, params, (err, addedRoleMessage) => {
+            if (err) {
+             console.log({ error: err.message });
+              return;
+            }
+            console.log(`Added a Role!`);
+            let sortByNewestFirst = true;
+            viewRoles(false, false, sortByNewestFirst);
+          });
+        }
+      }
+    })
+  });
+};
+
 const askEmployeeQuestionsAndThenAddEmployee = () => {
   const sql = `SELECT * FROM roles; SELECT * FROM employees; SELECT * FROM departments;`;
 
@@ -373,22 +494,6 @@ const askEmployeeQuestionsAndThenAddEmployee = () => {
     })
   });
 }
-
-
-// const addDepartment
-
-// const addRole
-
-// const askEmployeeQuestionsAndThenAddEmployee
-
-// inquirer.prompt(questionsToAsk)
-
-// const updateAnEmployeeRole
-
-// const mainMenuChoices
-
-// const startMenu
-
 
 const mainMenuChoices = {
   ViewAllDepartments: `View all departments`,
